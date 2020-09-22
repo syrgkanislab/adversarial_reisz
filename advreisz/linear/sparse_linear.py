6f3fdff2232a58144b4919233bad6142f5fd4590
Vasilis <vasy@microsoft.com>
2020-09-22 10:51:57 -0400

    stabilitized some numerical stuff in sparse linear. Still there is a corner case instability when the sparse linear trains for uber-uber-long training iterations.

--- advreisz/linear/sparse_linear.py
+++ advreisz/linear/sparse_linear.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.linear_model import Lasso, LassoCV, ElasticNet
 from sklearn.base import clone
 from sklearn.preprocessing import PolynomialFeatures
+from .utilities import cross_product
+import scipy.special
 
 
 class _SparseLinearAdvRiesz:
@@ -27,26 +29,36 @@ class _SparseLinearAdvRiesz:
     def coef(self):
         return self.coef_
 
-    def _check_duality_gap(self, X, moment_vec):
+    def _covs(self, X, coef, w, cov=None):
+        if cov is not None:
+            pred_cov = cov @ coef
+            test_cov = cov @ w
+        else:
+            pred_cov = np.mean(
+                X * np.dot(X, coef).reshape(-1, 1), axis=0)
+            test_cov = np.mean(X * np.dot(X, w).reshape(-1, 1), axis=0)
+        return pred_cov, test_cov
+
+    def _check_duality_gap(self, X, moment_vec, cov=None):
+        pred_cov, test_cov = self._covs(X, self.coef_, self.w_, cov)
         self.max_response_loss_ = np.linalg.norm(
-            moment_vec - np.mean(X * np.dot(X, self.coef_).reshape(-1, 1), axis=0), ord=np.inf)\
+            moment_vec - pred_cov, ord=np.inf)\
             + self.lambda_theta * np.linalg.norm(self.coef_, ord=1)
         self.min_response_loss_ = np.dot(moment_vec, self.w_) + self.B * np.clip(self.lambda_theta
-                                                                                 - np.linalg.norm(np.mean(X * np.dot(X, self.w_).reshape(-1, 1),
-                                                                                                          axis=0),
+                                                                                 - np.linalg.norm(test_cov,
                                                                                                   ord=np.inf),
                                                                                  - np.inf, 0)
         self.duality_gap_ = self.max_response_loss_ - self.min_response_loss_
         return self.duality_gap_ < self.tol
 
-    def _post_process(self, X, moment_vec):
+    def _post_process(self, X, moment_vec, cov=None):
         if self.sparsity is not None:
             thresh = 1 / (self.sparsity * (X.shape[0])**(2 / 3))
             filt = (np.abs(self.coef_) < thresh)
             self.coef_[filt] = 0
         self.max_violation_ = np.linalg.norm(
             moment_vec - np.mean(X * np.dot(X, self.coef_).reshape(-1, 1), axis=0), ord=np.inf)
-        self._check_duality_gap(X, moment_vec)
+        self._check_duality_gap(X, moment_vec, cov)
 
     def get_feature_names(self):
         return self.featurizer.get_feature_names()
@@ -60,7 +72,7 @@ class SparseLinearAdvRiesz(_SparseLinearAdvRiesz):
         X = self.featurizer.fit_transform(X)
         moment_vec = np.mean(np.array([self.moment_fn(Xraw, lambda x: self.featurizer.transform(x)[:, i])
                                        for i in range(X.shape[1])]), axis=1)
-
+        n = X.shape[0]
         d_x = X.shape[1]
         B = self.B
         T = self.n_iter
@@ -68,6 +80,11 @@ class SparseLinearAdvRiesz(_SparseLinearAdvRiesz):
         eta_w = .5 if self.eta_w == 'auto' else self.eta_w
         lambda_theta = self.lambda_theta
 
+        cov = np.mean(cross_product(X, X), axis=0).reshape(
+            d_x, d_x) if d_x < n else None
+
+        self.log_theta_list = []
+        self.log_w_list = []
         last_gap = np.inf
         t = 1
         while t < T:
@@ -75,36 +92,40 @@ class SparseLinearAdvRiesz(_SparseLinearAdvRiesz):
             if t == 2:
                 self.duality_gaps = []
                 theta = np.ones(2 * d_x) * B / (2 * d_x)
+                log_theta = np.zeros(2 * d_x)
                 theta_acc = np.ones(2 * d_x) * B / (2 * d_x)
                 w = np.ones(2 * d_x) / (2 * d_x)
+                log_w = np.zeros(2 * d_x)
                 w_acc = np.ones(2 * d_x) / (2 * d_x)
                 res = np.zeros(2 * d_x)
                 res_pre = np.zeros(2 * d_x)
                 cors = 0
 
+            pred_cov, test_cov = self._covs(
+                X, theta[:d_x] - theta[d_x:], w[:d_x] - w[d_x:], cov)
+
             # quantities for updating theta
-            test_fn = np.dot(X, w[:d_x] - w[d_x:]).reshape(-1, 1)
-            cors_t = - np.mean(test_fn * X, axis=0)
+            cors_t = - test_cov
             cors += cors_t
 
             # quantities for updating w
-            pred_fn = np.dot(X, theta[:d_x] - theta[d_x:]).reshape(-1, 1)
-            res[:d_x] = moment_vec - np.mean(pred_fn * X, axis=0)
+            res[:d_x] = moment_vec - pred_cov
             res[d_x:] = - res[:d_x]
 
             # update theta
-            theta[:d_x] = np.exp(-1 - eta_theta *
-                                 (cors + cors_t + (t + 1) * lambda_theta))
-            theta[d_x:] = np.exp(-1 - eta_theta *
-                                 (- cors - cors_t + (t + 1) * lambda_theta))
-            normalization = np.sum(theta)
-            if normalization > B:
-                theta[:] = theta * B / normalization
+            log_theta[:d_x] = -1 - eta_theta * \
+                (cors + cors_t + (t + 1) * lambda_theta)
+            log_theta[d_x:] = -1 - eta_theta * \
+                (- cors - cors_t + (t + 1) * lambda_theta)
+            normalization = scipy.special.logsumexp(log_theta)
+            if normalization > np.log(B):
+                log_theta[:] = log_theta + np.log(B) - normalization
+            theta[:] = np.exp(log_theta)
 
             # update w
-            w[:] = w * \
-                np.exp(2 * eta_w * res - eta_w * res_pre)
-            w[:] = w / np.sum(w)
+            log_w[:] = log_w + 2 * eta_w * res - eta_w * res_pre
+            log_w[:] = log_w - scipy.special.logsumexp(log_w)
+            w[:] = np.exp(log_w)
 
             theta_acc = theta_acc * (t - 1) / t + theta / t
             w_acc = w_acc * (t - 1) / t + w / t
@@ -113,7 +134,7 @@ class SparseLinearAdvRiesz(_SparseLinearAdvRiesz):
             if t % 50 == 0:
                 self.coef_ = theta_acc[:d_x] - theta_acc[d_x:]
                 self.w_ = w_acc[:d_x] - w_acc[d_x:]
-                if self._check_duality_gap(X, moment_vec):
+                if self._check_duality_gap(X, moment_vec, cov):
                     break
                 self.duality_gaps.append(self.duality_gap_)
                 if np.isnan(self.duality_gap_):
@@ -124,12 +145,14 @@ class SparseLinearAdvRiesz(_SparseLinearAdvRiesz):
                     eta_theta /= 1.01
                     eta_w /= 1.01
                 last_gap = self.duality_gap_
+                self.log_theta_list.append(log_theta.copy())
+                self.log_w_list.append(log_w.copy())
 
         self.n_iters_ = t
         self.rho_ = theta_acc
         self.coef_ = theta_acc[:d_x] - theta_acc[d_x:]
         self.w_ = w_acc[:d_x] - w_acc[d_x:]
 
-        self._post_process(X, moment_vec)
+        self._post_process(X, moment_vec, cov)
 
         return self
