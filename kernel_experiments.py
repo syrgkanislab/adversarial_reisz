1000a85eba31a2aab8783be30aa56e8c5780469c
Vasilis Syrgkanis <bsyrganis@gmail.com>
2021-09-02 21:07:49 -0400

    added option for product kernel

--- kernel_experiments.py
+++ kernel_experiments.py
@@ -13,6 +13,16 @@ import os
 import argparse
 from advreisz.kernel import KernelReisz, AdvKernelReisz
 
+
+def binary_kernel(X, Y=None):
+    return 1.0 * (X[:, [0]] == X[:, [0]].T) if Y is None else 1.0 * (X[:, [0]] == Y[:, [0]].T)
+
+def prod_kernel(X, Y=None, *, gamma):
+    if Y is None:
+        return rbf_kernel(X[:, 1:], gamma=gamma) * binary_kernel(X, Y)
+    else:
+        return rbf_kernel(X[:, 1:], Y[:, 1:], gamma=gamma) * binary_kernel(X, Y)
+
 def moment_fn(x, test_fn):
     t1 = test_fn(np.hstack([np.ones((x.shape[0], 1)), x[:, 1:]]))
     t0 = test_fn(np.hstack([np.zeros((x.shape[0], 1)), x[:, 1:]]))
@@ -57,9 +67,13 @@ def exp(it, splin_fn, n_samples=None):
     return mean_ci(moment_fn(X, est.predict) + a_test * (y - est.predict(X)))
 
 
-def all_experiments(n_samples_list, target_dir = '.'):
+def all_experiments(n_samples_list, target_dir = '.', kernelid=0):
 
-    kernel = lambda X, Y=None: rbf_kernel(X, Y=Y, gamma=.1)
+    if kernelid == 0:
+        kernel = lambda X, Y=None: rbf_kernel(X, Y=Y, gamma=.1)
+    else:
+        kernel = lambda X, Y=None: prod_kernel(X, Y=Y, gamma=.1)
+
     true = 2.2
 
     for n_samples in n_samples_list:
@@ -80,5 +94,6 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-n_samples", "--n_samples", type=int)
+    parser.add_argument("-kernel", "--kernel", type=int, default=0)
     args = parser.parse_args()
-    all_experiments([args.n_samples], os.environ['AMLT_OUTPUT_DIR'])
+    all_experiments([args.n_samples], os.environ['AMLT_OUTPUT_DIR'], args.kernel)
